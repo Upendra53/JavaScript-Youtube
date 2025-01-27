6af5288b1509e6f5f24e36087cca4b9f99f1c89e
Upendra53 <126756828+Upendra53@users.noreply.github.com>
2025-01-27 13:22:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)  //we are using node js not browser
+
+
+let name="upendra yadav"
+let age=20
+let isLoggedIn=false
+
+
+//number=>2^53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+
+
+console.log(typeof age);
