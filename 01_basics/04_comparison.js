a86785031c79278f7fb9c383b369b7c73bcdc9f9
Upendra53 <126756828+Upendra53@users.noreply.github.com>
2025-01-28 11:03:10 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log("2">1);    //true
+console.log("b">"c");  //false
+console.log("4"<"5");  //true
+console.log(null==0);  //flase
+console.log(2<3);      //true
+
+console.log(null>0);    //false
+console.log(null>=0);    //true
+console.log(null==0);   //false
+
+console.log(undefined==0); //false
+
+//=== 
+//strict check ,it check data type as well as
+console.log("2"===2);
